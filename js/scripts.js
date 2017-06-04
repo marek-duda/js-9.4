c3ab58a5c249d054fbe66113298704af3401790b
Maro <grafika.marekduda@gmail.com>
2017-06-05 00:13:33 +0200

    New js project 9.4

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+function drawTree (height) {
+
+  for (var i = 1; i <= height; i++) {
+    var star = '';
+
+    for (var j = 1; j <= i; j++) {
+      star += '*';
+    }
+
+    console.log(star);
+  }
+}
+
+drawTree(5);
