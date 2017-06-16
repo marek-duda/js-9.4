d0bcc05952652541cc8012596d24308c92af9e83
Maro <grafika.marekduda@gmail.com>
2017-06-16 22:17:38 +0200

    symetric tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,12 @@
 function drawTree (height) {
   for (var i = 1; i <= height; i++) {
     var line = '';
-    for (var j = 1; j <= i; j++) {
-      line += '*';
+      
+      for (var k = i; k <= height; k++){
+        line += ' ';
+      }   
+       for (var j = 1; j <= i * 2 - 1; j++) {
+         line += '*';
     }
     console.log(line);
   }
