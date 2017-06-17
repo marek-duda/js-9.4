e92ab8e9a0852bd3143fa5db7be6b13a6ccc1aae
Maro <grafika.marekduda@gmail.com>
2017-06-18 00:32:22 +0200

    new christmas tree

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,14 @@
 function drawTree (height) {
-  for (var i = 1; i <= height; i++) {
-    var line = '';
-      
-      for (var k = i; k <= height; k++){
-        line += ' ';
-      }   
-       for (var j = 1; j <= i * 2 - 1; j++) {
-         line += '*';
+    for (var i = 0; i < height; i++) {
+        var line = '';
+        for (var k = i; k < height - 1; k++){
+            line += ' ';
+        }   
+        for (var j = 0; j < i * 2 - 1; j++) {
+            line += '*';
+        }
+        console.log(line);
     }
-    console.log(line);
-  }
 }
 
 drawTree(5);
